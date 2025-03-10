b706af0a8ede015e46449365cb50d986755a55ab
V1p3r <juanfra.riera2003@gmail.com>
2025-03-10 10:19:34 +0100

    Delete react-whatsapp-web-master directory

--- react-whatsapp-web-master/src/feature/pages/homepage/utils/LeftPanel.types.ts
+++
@@ -1,6 +0,0 @@
-export interface ChatCardType {
-  name: string;
-  lastText: string;
-  lastSeen: string;
-  selected: boolean;
-}
--- react-whatsapp-web-master/src/feature/pages/homepage/utils/constant.ts
+++
@@ -1,2 +0,0 @@
-export const rightPanelMenuItem = ["Información del contacto", "Business Details"];
-export const leftPanelMenuItem = ["Información del usuario", "Business Details"];
